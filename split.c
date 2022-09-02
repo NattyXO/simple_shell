7f12f30b65aba120a7cb5209394e95ec5da70fb4
NAttyXO <nattyxo2018@gmail.com>
2022-09-02 11:06:37 -0700

    [Feature]: adding replacement of variables and fixing operators and comments

--- split.c
+++ split.c
@@ -9,7 +9,6 @@ char **_strtok(char *line, char *delim);
 *             of the first token contained within a string.
 * @str: The string to be searched.
 * @delim: The delimiter character.
-*
 * Return: The delimiter index marking the end of
 *         the intitial token pointed to be str.
 */
@@ -31,6 +30,7 @@ return (len);
 *                words contained within a string.
 * @str: The string to be searched.
 * @delim: The delimiter character.
+*
 *
 * Return: The number of words contained within str.
 */
